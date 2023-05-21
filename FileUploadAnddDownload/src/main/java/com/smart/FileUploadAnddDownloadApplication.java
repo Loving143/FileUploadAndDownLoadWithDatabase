7b0105b9dbbf269698bfd14b6b533e736d4ccca7
Prateek <Prateek.kumar@gmail.com>
2023-05-21 18:17:30 +0530

    This is file upload and Download with the database and not with the FileSystem

---
+++ FileUploadAnddDownload/src/main/java/com/smart/FileUploadAnddDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadAnddDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadAnddDownloadApplication.class, args);
+	}
+
+}
